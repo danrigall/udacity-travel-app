6dfdab1e80abeb244addd9aed3d3b90b1b16f6f9
Dan Rigall <danrigall@gmail.com>
2021-01-15 18:29:25 -0800

    fix: (cleans up some server errors)

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -3,7 +3,7 @@ function updateUI(data) {
   console.log('all data returned is:')
   console.log(data)
   const geonames = data.geo
-  const weatherbit = data.weather
+  const weatherbit = Math.round(data.weather.precip * 10) / 10
   const pixabay = data.pix
   const postHolder = document.getElementById('all-posts')
   const newDiv = document.createElement('div');
@@ -14,7 +14,7 @@ function updateUI(data) {
           <div class="wait-time"><i>${geonames.name}, ${Client.checkCountry(geonames)} is ${Client.dateDiff()} days away.</i></div>
           <h3>Typical weather for then is:</h3>
           <div class="temp">High: ${weatherbit.max_temp}&degC, Low: ${weatherbit.min_temp}&degC</div>
-          <div class="precip">With a chance for ${weatherbit.precip}mm of precipitation</div>
+          <div class="precip">With a chance for ${weatherbit}mm of precipitation</div>
       </div>
   `;
   postHolder.appendChild(newDiv);
--- src/server/server.js
+++ src/server/server.js
@@ -35,6 +35,23 @@ const server = app.listen(port, () => {
   console.log(`SERVER IS RUNNING on localhost: ${port}`)
 })
 
+const checkPix = async (geo) => {
+  let pixObj = await getPix(encodeURI(geo.name + ', ' + geo.adminName1))
+  console.log('*** CHECKING FOR PICTURE ***')
+  const geoArr = [geo.adminName1, geo.countryName]
+  let i = 0
+  while (pixObj === undefined) {
+    pixObj = await getPix(encodeURI(geoArr[i]))
+    console.log('pixObj was checked & returned as:')
+    console.log(pixObj)
+    i++
+    if (i > 1) {
+      break;
+    }
+  }
+  return pixObj
+}
+
 // Direct PUT data to inputData array
 const handleInput = async (req, res) => {
   const userInput = {
@@ -42,42 +59,38 @@ const handleInput = async (req, res) => {
     date: req.body.date
   }
   // ***
-  // TODO: FIGURE OUT HOW TO HANDLE DIFFERENT LOCATIONS & INPUTS!!!!!!!!!!!!!!!
+  // TODO: FIGURE OUT HOW TO HANDLE DIFFERENT LOCATIONS & INPUTS (IE. CZECH REPUBLIC INSTEAD OF CZECHIA)!!!!!!!!!!!!!!!
   // ***
-
   const geoObj = await getGeonames(userInput)
   const weatherObj = await getWeather(geoObj)
-  let pixObj = await getPix(geoObj.name, geoObj.adminName1)
-  console.log('*** CHECKING FOR PICTURE ***')
-  if (pixObj === undefined) {
-    pixObj = await getPix(geoObj.adminName1, geoObj.countryName)
-    console.log('pixObj was checked & changed to:')
-    console.log(pixObj)
+  const pixObj = await checkPix(geoObj)
+  try {
+    const allData = {
+      geo: geoObj,
+      weather: weatherObj,
+      pix: pixObj,
+    }
+    res.send(allData)
+  } catch (error) {
+    console.log(error)
+    res.send(error)
   }
-
-  const allData = {
-    geo: geoObj,
-    weather: weatherObj,
-    pix: pixObj,
-  }
-
-  res.send(allData)
 }
 
 // Set up POST route
 app.post('/add', handleInput);
 
-// Set up GET route
 // ***THIS IS CURRENTLY UNUSED***
-app.get('/all', (req, res));
+// app.get('/all', (req, res));
 
 // Fetch from Geonames API
 const getGeonames = async (input) => {
   const geoKey = process.env.GEO_KEY
   console.log('Your geoKey is: ' + geoKey)
   const geoBaseURL = 'http://api.geonames.org/searchJSON?q='
-  const geoAddURL = '&fuzzy=0.8&maxRows=10&username='
+  const geoAddURL = '&fuzzy=0.6&maxRows=10&username='
   console.log('input location is: ' + input.location)
+  console.log(geoBaseURL + input.location + geoAddURL + geoKey)
   const request = await fetch(geoBaseURL + input.location + geoAddURL + geoKey)
   try {
     const allGeo = await request.json()
@@ -113,19 +126,17 @@ const getWeather = async (geo) => {
 }
 
 // Fetch from Pixabay API
-const getPix = async (place, area) => {
-  const geoURI = encodeURI(`${place}, ${area}`)
-  console.log('geoData made it to getPix: ' + geoURI)
+const getPix = async (place) => {
+  console.log('geoData made it to getPix: ' + place)
   const pixKey = process.env.PIX_KEY
-  console.log('Your pixKey is: ' + pixKey)
-  const pixURL = `https://pixabay.com/api/?key=${pixKey}&q=${geoURI}&image_type=photo&orientation=horizontal&safesearch=true&category=travel&category=places&category=buildings`
+  const pixURL = `https://pixabay.com/api/?key=${pixKey}&q=${place}&image_type=photo&orientation=horizontal&safesearch=true&category=travel&category=places&category=buildings`
+  console.log(pixURL)
   const fetchPix = await fetch(pixURL);
-  console.log('*** CHECKING FOR PICTURE ***')
   try {
-    const allPix = await fetchPix.json()
+    const results = await fetchPix.json()
     console.log('Output of getPix.hits[0] is:')
-    console.log(allPix.hits[0])
-    return allPix.hits[0]
+    console.log(results.hits[0])
+    return results.hits[0]
   } catch (error) {
     console.log("ERROR in Pix GET:", error);
   }
